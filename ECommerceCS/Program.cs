0ea0a9d5848928ec9369801932f7e57040f5f037
gjianhui <gjianhui@gmail.com>
2016-03-11 14:25:59 +0800

    New Console Application

---
+++ ECommerceCS/Program.cs
@@ -0,0 +1,41 @@
+﻿using PromotionCS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ECommerceCS
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Order order;
+            Console.WriteLine("Press 1 to enable/disable discount.");
+            Console.WriteLine("Press 2 change discount amount for any dates.");
+            Console.WriteLine("Press 3 to add product to shopping cart.");
+            Console.WriteLine("Press 4 to display total price.");
+            Console.WriteLine("Press 5 to display shopping cart.");
+
+            String x = Console.ReadLine();
+            //Console.WriteLine("X"+(char)x);
+            if (x.Equals("1"))
+            {
+                Console.WriteLine("Entered to enable/disable discount.");
+            }
+            else if (x.Equals("2"))
+            {
+                Console.WriteLine("Entered to enable/disable discount.");
+            }
+            else if (x.Equals("3"))
+            {
+                order = new Order();
+                Console.WriteLine("Shopping Cart Created.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
